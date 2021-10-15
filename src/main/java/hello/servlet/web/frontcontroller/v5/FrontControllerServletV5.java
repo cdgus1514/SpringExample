24625b4ac613833c07fb4c68e43ecb6dd9bd3b87
choidh <cdgus1514@gmail.com>
2021-10-15 22:39:22 +0900

    어댑터패턴 추가
    - 다양한 방식의 컨트롤러를 처리할 수 있도록 변경
    - 핸들러어댑터, 핸들러 추가(ControllerV3 사용)

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,109 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    // 기존
+    //private  Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    // 변경(object로 받아서 어떤 컨트롤러나 모두 들어올 수 있음)
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        // 핸들러어답터 초기화
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV5.service");
+
+        /**
+         * 프론트컨트롤러 v5
+         *
+         * 1. HTTP 요청
+         * 2. 핸들러 맵핑정보 조회
+         * 3. 핸들러를 처리할 수 있는 핸들러어뎁터 조회
+         * 4. handle(handler) -> 핸들러어뎁터 호출
+         * 5. 핸들러(컨트롤러) 호출, 모델뷰 반환
+         * 6. ViewResolver 호출 후 Myview 반환
+         * 7. FrontController에서 render(model) 호출
+         * 8. HTML 응답
+         */
+
+
+        // 요청정보를 가지고 핸들러 호출
+        Object handler = getHandler(request);
+
+        if(handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 찾은 핸들러로 핸들어어답터 찾기
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView mv = adapter.handler(request, response, handler);
+
+
+        // 논리이름을 얻어서 물리이름생성
+        String viewname = mv.getViewname();
+        MyView view = viewResolver(viewname);
+
+        // 모델을 같이 넘겨줘야 함
+        view.render(mv.getModel(), request, response);
+    }
+
+
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if(adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler =" + handler);
+    }
+
+    private MyView viewResolver(String viewname) {
+        return new MyView("/WEB-INF/views/" + viewname + ".jsp");
+    }
+}
+
